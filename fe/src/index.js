3b5d00f072636ab920a2c2b124117cde50af12a7
Quang Dung <ntquangdung2001@gmail.com>
2023-08-26 11:07:42 +0700

    create Account

--- fe/src/index.js
+++ fe/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Signin from '../src/layouts/Signin'
 import reportWebVitals from './reportWebVitals';
+import SignUp from './layouts/SignUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Signin/>
+    <SignUp/>
   </React.StrictMode>
 );
 
